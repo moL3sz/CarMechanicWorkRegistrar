710e49dea59c4e09050102cb890b40b0553444bd
Molnár Kristóf Ferenc <moleszkf@gmail.com>
2023-04-22 20:12:12 +0200

    Suffering by CORS Chapter #2, failed to added AddCors();

--- WorkRegistrarAPI/Program.cs
+++ WorkRegistrarAPI/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddDbContext<PitStopContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
