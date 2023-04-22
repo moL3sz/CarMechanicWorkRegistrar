6fec531ea1cf9df6a23beaa2b178d64fb2b96212
Lélek Hunor Kadosa <lelekkadosa@gmail.com>
2023-04-22 20:11:54 +0200

    Debugged the CORS problem and Cleaned the workflow from attribute

--- CarMechanicClient/Models/Workflow.cs
+++ CarMechanicClient/Models/Workflow.cs
@@ -1,40 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using CarMechanicClient.Enums;
 
 namespace CarMechanicClient.Models
 {
-    public class WorkFlow
+    public class Workflow
     {
-        [DisplayName("Vezetéknév")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
-        public string FirstName { get; set; }
 
-        [DisplayName("Keresztnév")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
-        public string LastName { get; set; }
+        public int WorkflowId { get; set; }
 
-        [DisplayName("Autó típusa")]
+        public string ClientFirstName { get; set; }
+
+        public string ClientLastName { get; set; }
+
         public string CarType { get; set; }
 
-        [DisplayName("Rendszám")]
-        public string LicensePlateNumber { get; set; }
+        public string LicencePlateNumber { get; set; }
 
-        [DisplayName("Gyártási év")]
-        [Required]
-        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
-        public int YearOfManufacture { get; set; }
+        public int ManuFactureYear { get; set; }
 
-        [DisplayName("Munka kategória")]
-        [Required]
-        public string WorkType { get; set; }
+        public WorkCatagory WorkCatagory;
 
-        [DisplayName("Hiba rövid leírása")]
-        [Required]
-        public string ProblemDescription { get; set; }
+        public WorkStatus WorkStatus { get; set; }
 
-        [DisplayName("Hiba súlyossága")]
-        [Required]
-        [Range(1, 10)]
-        public int HowBadIsIt { get; set; }
+        public string Description { get; set; }
+
+        public short IssueSeriousness { get; set; }
+
+        public bool Active { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int CarAge { get; set; }
+
+        public double WorktimeEstimination { get; set; }
     }
 }
