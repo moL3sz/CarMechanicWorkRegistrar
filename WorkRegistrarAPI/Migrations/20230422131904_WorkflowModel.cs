f89ab271f96c8da035198d23f3f249fa2f580968
Molnár Kristóf Ferenc <moleszkf@gmail.com>
2023-04-22 15:31:17 +0200

    Database context initalized with the Workflow DbSet. Can query from database now.

---
+++ WorkRegistrarAPI/Migrations/20230422131904_WorkflowModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkRegistrarAPI.Migrations
+{
+    public partial class WorkflowModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workflows",
+                columns: table => new
+                {
+                    WorkflowId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientFirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientLastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CarType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LicencePlateNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManuFactureYear = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IssueSeriousness = table.Column<short>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workflows", x => x.WorkflowId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workflows");
+        }
+    }
+}
