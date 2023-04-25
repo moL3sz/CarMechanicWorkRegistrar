71cf2070651b5af39a3ce21fcb8e6d6878dc151b
Lélek Hunor Kadosa <lelekkadosa@gmail.com>
2023-04-25 02:33:00 +0200

    Added the PageManager

---
+++ CarMechanicClient/Managers/PageManager.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace CarMechanicClient.Managers
+{
+    class PageManager
+    {
+        public int maxElement { get; set; }
+        public int maxPage { get; set; }
+        public int currentPage { get; set; }
+        public int pageSize { get; set; }
+
+
+        public PageManager()
+        {
+            pageSize = 10;
+            currentPage = 1;
+        }
+
+        public void calcMaxPage()
+        {
+            maxPage = maxElement / pageSize;
+            currentPage = 1;
+        }
+
+        public async Task changePage(MouseEventArgs e, int change, Func<Task> callback)
+        {
+            if (currentPage + change >= 1 && currentPage + change <= maxPage) currentPage += change;
+            await callback();
+        }
+
+        public string showPageCount()
+        {
+            return currentPage + " / " + maxPage;
+        }
+
+        public async Task setPageSize(string value, Func<Task> callback)
+        {
+            pageSize = int.Parse(value);
+            calcMaxPage();
+            await callback();
+        }
+    }
+}
