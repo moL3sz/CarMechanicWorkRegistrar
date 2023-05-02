efb8b36c1af5b6dbbf8dc0cf34cd15badbff6aa5
Adrián <2001adrian.fekete@gmail.com>
2023-05-02 17:32:24 +0200

    pagemanager

---
+++ WorkManagerClient/Managers/PageManager.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace WorkManagerClient.Managers
+{
+    class PageManager
+    {
+        public int maxElement { get; set; }
+        public int maxPage { get; set; }
+        public int currentPage { get; set; }
+        public int pageSize { get; set; }
+
+
+        public PageManager()
+        {
+            pageSize = 10;
+            currentPage = 1;
+        }
+
+        public void calcMaxPage()
+        {
+            maxPage = maxElement / pageSize;
+            if (maxElement % pageSize != 0) maxPage++;
+            currentPage = 1;
+        }
+
+        public async Task changePage(MouseEventArgs e, int change, Func<Task> callback)
+        {
+            if (currentPage + change >= 1 && currentPage + change <= maxPage) currentPage += change;
+            await callback();
+        }
+
+        public string showPageCount()
+        {
+            return currentPage + " / " + maxPage;
+        }
+
+        public async Task setPageSize(string value, Func<Task> callback)
+        {
+            pageSize = int.Parse(value);
+            calcMaxPage();
+            await callback();
+        }
+    }
+}
