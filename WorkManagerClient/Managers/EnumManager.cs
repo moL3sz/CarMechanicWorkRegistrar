5fdbd36d4e0c8ca50d3d5ce802a4002c059e1dde
Molnár Kristóf Ferenc <moleszkf@gmail.com>
2023-06-07 20:02:29 +0200

    styling a bit

---
+++ WorkManagerClient/Managers/EnumManager.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace WorkManagerClient.Managers
+{
+    public static class EnumManager
+    {
+        public static string GetDescription(this Enum value)
+        {
+            string desc = value.GetType()
+                            .GetMember(value.ToString())
+                            .First()
+                            .GetCustomAttribute<DescriptionAttribute>().Description;
+
+            return desc;
+
+
+        }
+    }
+}
