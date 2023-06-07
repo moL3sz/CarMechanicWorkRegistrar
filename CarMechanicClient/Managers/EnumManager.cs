fe4242ab756eb7f3af352b4e78857fb79e095b53
Molnár Kristóf Ferenc <moleszkf@gmail.com>
2023-06-07 20:46:47 +0200

    Stlying more with bootstrap

---
+++ CarMechanicClient/Managers/EnumManager.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CarMechanicClient.Managers
+{
+    public static class EnumManager
+    {
+        public static string GetDescription(this Enum value)
+        {
+            string desc = value.GetType()
+                            .GetMember(value.ToString())
+                            .First()
+                            .GetCustomAttribute<DescriptionAttribute>().Description;
+
+            return desc;
+
+
+        }
+    }
+}
